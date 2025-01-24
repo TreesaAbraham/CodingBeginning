f4328c032b46748088159050c1ffe8e1758623d2
Treesa Abraham <treesa.abraham106@gmail.com>
2025-01-24 07:17:58 -0500

    update

--- src/home.js
+++ src/home.js
@@ -1,7 +1,15 @@
 const  Home = () => {
+    const handleClick = (e) => { 
+        console.log('hello, book nerds', e);
+    }
+    const handleClickAgain = (name)  => { 
+        console.log('hello ' + name);
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}> Click Me</button>
+            <button onClick={() =>  handleClickAgain('mario') }> Click Me again</button>
         </div>
      );
 }
