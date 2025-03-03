f6d3e7d6d301019d170c1cd0b6428efee2a9dac2
Treesa Abraham <treesa.abraham106@gmail.com>
2025-03-03 15:14:31 -0500

    inputs

--- src/create.js
+++ src/create.js
@@ -1,24 +1,49 @@
+import {useState} from 'react';
+
 const  Create = () => {
+    const [title, setTitle] = useState(''); 
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('harry');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            const blog = {title, body, author};
+            console.log(blog);
+    }
+}
     return (
-        <div>
+        <div className = "create">
             <h2>Create a new blog</h2>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <label>Blog title:</label>
                 <input 
                 type="text" 
                 required 
+                value = {title}
+                onChange={(e) => setTitle(e.target.value)}
                 /> 
                 <label>Blog body:</label>
-                <textarea>
+                <textarea
                    required
-
-                </textarea>
+                   value = {body}
+                onChange={(e) => setBody(e.target.value)}
+                ></textarea>
                 <label>Author:</label>
-                <select>
-                    <option value="mario">Mario</option>
-                    <option value="yoshi">Yoshi</option>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Harry">Harry</option>
+                    <option value="Ron">Ron</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     );
