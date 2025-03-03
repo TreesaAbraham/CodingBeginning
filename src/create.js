bd5b6e4c5f89c3b419e2fa7b55e60f4a0dd48209
Treesa Abraham <treesa.abraham106@gmail.com>
2025-03-03 14:43:29 -0500

    message

--- src/create.js
+++ src/create.js
@@ -2,6 +2,24 @@ const  Create = () => {
     return (
         <div>
             <h2>Create a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                type="text" 
+                required 
+                /> 
+                <label>Blog body:</label>
+                <textarea>
+                   required
+
+                </textarea>
+                <label>Author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
